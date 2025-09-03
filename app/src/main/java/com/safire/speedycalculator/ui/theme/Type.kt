ce76395872a729d9608b5753b1862b1a846daef5
Safire <76739907+MainActivityKt@users.noreply.github.com>
2025-09-04 00:01:08 +0430

    Added vend sans font

--- app/src/main/java/com/safire/speedycalculator/ui/theme/Type.kt
+++ app/src/main/java/com/safire/speedycalculator/ui/theme/Type.kt
@@ -1,24 +1,28 @@
 package com.safire.speedycalculator.ui.theme
 
-import android.R.attr.baseline
 import androidx.compose.material3.Typography
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.safire.speedycalculator.R
 
-val bodyFontFamily =FontFamily(
+val vendSans =FontFamily(
     Font(R.font.vend_sans)
 )
 val AppTypography = Typography(
-    bodyLarge = Typography().bodyLarge.copy(fontFamily = bodyFontFamily),
-    bodyMedium = Typography().bodyMedium.copy(fontFamily = bodyFontFamily),
-    bodySmall = Typography().bodySmall.copy(fontFamily = bodyFontFamily),
-    labelLarge = Typography().labelLarge.copy(fontFamily = bodyFontFamily),
-    labelMedium = Typography().labelMedium.copy(fontFamily = bodyFontFamily),
-    labelSmall = Typography().labelSmall.copy(fontFamily = bodyFontFamily),
+    titleMedium = Typography().titleMedium.copy(
+        fontFamily = vendSans,
+        fontWeight = FontWeight.Medium,
+        fontSize = 28.sp,
+        letterSpacing = 4.sp
+    ),
+    bodyLarge = Typography().bodyLarge.copy(fontFamily = vendSans),
+    bodyMedium = Typography().bodyMedium.copy(fontFamily = vendSans, fontSize = 44.sp),
+    bodySmall = Typography().bodySmall.copy(fontFamily = vendSans),
+    labelLarge = Typography().labelLarge.copy(fontFamily = vendSans),
+    labelMedium = Typography().labelMedium.copy(fontFamily = vendSans),
+    labelSmall = Typography().labelSmall.copy(fontFamily = vendSans),
 
 
 )
