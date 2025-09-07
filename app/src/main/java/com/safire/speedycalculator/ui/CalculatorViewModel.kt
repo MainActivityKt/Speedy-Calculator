d2c7cf44c51455a777f9ecee79b204fdfc6c879c
Safire <76739907+MainActivityKt@users.noreply.github.com>
2025-09-07 16:01:30 +0430

    - Added more conditions to make sure the entered expression is correct before getting the result

--- app/src/main/java/com/safire/speedycalculator/ui/CalculatorViewModel.kt
+++ app/src/main/java/com/safire/speedycalculator/ui/CalculatorViewModel.kt
@@ -2,6 +2,7 @@ package com.safire.speedycalculator.ui
 
 import androidx.core.text.isDigitsOnly
 import androidx.lifecycle.ViewModel
+import com.safire.speedycalculator.calculator.Calculator
 import com.safire.speedycalculator.model.CalculatorUiState
 import com.safire.speedycalculator.model.KeyCategory
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -17,23 +18,24 @@ class CalculatorViewModel : ViewModel() {
 
     private var lastEntryHasDecimal = false
     private var openingParenthesisCount = 0
-
+    private val calculator = Calculator()
 
 
     fun onButtonClick(button: CalculatorButton) {
 
         val lastValue =
-            if (_uiState.value.enteredEquation.isNotEmpty()) _uiState.value.enteredEquation.last() else ' '
+            if (_uiState.value.enteredExpression.isNotEmpty()) _uiState.value.enteredExpression.last() else ' '
 
         when (button.category) {
             KeyCategory.OPERATOR -> {
 
-                if (lastValue.isDigit()) {
+                if (lastValue.isDigit() || lastValue.isOpeningParenthesis() || lastValue.isClosingParenthesis()) {
                     addToEquation(button.text)
                 } else if (lastValue.isOperator()) {
                     changeLastValue(button.text)
                 }
             }
+
             KeyCategory.DECIMAL -> {
                 if (!lastEntryHasDecimal) {
                     // if decimal is the first digit or comes after a non-digit item, change it to 0.
@@ -50,17 +52,19 @@ class CalculatorViewModel : ViewModel() {
                         addToEquation(CLOSING_PARENTHESIS)
                         openingParenthesisCount--
                     } else {
-                        addToEquation(OPENING_PARENTHESIS)
+                        addToEquation("x$OPENING_PARENTHESIS")
                         openingParenthesisCount++
                     }
-                }
-                else if (lastValue.isWhitespace() || lastValue.isOperator() || lastValue.isOpeningParenthesis()) {
+                } else if (lastValue.isWhitespace() || lastValue.isOperator() || lastValue.isOpeningParenthesis()) {
                     addToEquation(OPENING_PARENTHESIS)
                     openingParenthesisCount++
                 }
             }
 
             KeyCategory.DIGIT -> {
+                if (lastValue.isClosingParenthesis()) {
+                    addToEquation("x${button.text}")
+                }
                 addToEquation(button.text)
             }
 
@@ -69,7 +73,32 @@ class CalculatorViewModel : ViewModel() {
 
             }
 
-            else -> addToEquation(button.text)
+            KeyCategory.EQUAL_SIGN -> {
+                if (!lastValue.isOperator()) {
+                    var result = calculator.calculate(_uiState.value.enteredExpression)
+                    result = "%,.10f".format(result.toDouble())
+                    // removes trailing zeros in decimal digits
+                    val formattedResult = result.replace(Regex("\\.?0*$"), "")
+
+                    _uiState.update {
+                        it.copy(result = formattedResult)
+                    }
+                    openingParenthesisCount = 0
+                    lastEntryHasDecimal = false
+                }
+            }
+
+            KeyCategory.NUMBER_SIGN -> {
+                val lasTwoDigits = _uiState.value.enteredExpression.takeLast(2)
+                if (lasTwoDigits != "(-") {
+                    addToEquation("(-")
+                    openingParenthesisCount++
+                } else {
+                    _uiState.update { it ->
+                        it.copy(enteredExpression = it.enteredExpression.dropLast(2))
+                    }
+                }
+            }
         }
 
 
@@ -80,26 +109,26 @@ class CalculatorViewModel : ViewModel() {
             lastEntryHasDecimal = false
         }
         _uiState.update {
-            it.copy(enteredEquation = it.enteredEquation + value)
+            it.copy(enteredExpression = it.enteredExpression + value, result = "")
         }
     }
 
     private fun changeLastValue(value: String) {
         _uiState.update {
-            it.copy(enteredEquation = it.enteredEquation.dropLast(1) + value)
+            it.copy(enteredExpression = it.enteredExpression.dropLast(1) + value)
         }
     }
 
     fun clearLast() {
         val lastValue =
-            (if (_uiState.value.enteredEquation.isNotEmpty()) _uiState.value.enteredEquation.last() else ' ').toString()
+            (if (_uiState.value.enteredExpression.isNotEmpty()) _uiState.value.enteredExpression.last() else ' ').toString()
 
-        when(lastValue) {
+        when (lastValue) {
             OPENING_PARENTHESIS -> openingParenthesisCount--
             CLOSING_PARENTHESIS -> openingParenthesisCount++
             "." -> lastEntryHasDecimal = false
         }
-        _uiState.update { it.copy(enteredEquation = it.enteredEquation.dropLast(1)) }
+        _uiState.update { it.copy(enteredExpression = it.enteredExpression.dropLast(1)) }
 
     }
 
