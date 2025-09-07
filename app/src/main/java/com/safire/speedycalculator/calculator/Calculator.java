4cf51f7bdfabf8b7bc4807e543cf68d58027ca83
Safire <76739907+MainActivityKt@users.noreply.github.com>
2025-09-07 16:03:20 +0430

    - Added some changes to the calculator to match the app's requirements. For example: Changed / to ÷

--- app/src/main/java/com/safire/speedycalculator/calculator/Calculator.java
+++ app/src/main/java/com/safire/speedycalculator/calculator/Calculator.java
@@ -1,70 +1,40 @@
 package com.safire.speedycalculator.calculator;
 
-import java.math.BigInteger;
-import java.util.*;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class Calculator {
 
-
-    static final String EXIT = "/exit";
-    static final String HELP = "/help";
     static final Pattern LEFT_PARENTHESIS = Pattern.compile("\\(");
     static final Pattern RIGHT_PARENTHESIS = Pattern.compile("\\)");
 
-    static Pattern SINGLE_NUMBER = Pattern.compile("[+-]*\\d+(.\\d+)?");
-    static final Pattern OPERATOR = Pattern.compile("[+\\-*÷%]|-+");
+    static Pattern SINGLE_NUMBER = Pattern.compile("-*\\d+(.\\d+)?");
+    static final Pattern OPERATOR = Pattern.compile("[+\\-x÷%]|-+");
 
-    static Map<String, Operator> operatorsPrecedence = new HashMap<>();
-    static Map<String, BigInteger> variables = new HashMap<>();
+    Map<String, Operator> operatorsPrecedence = new HashMap<>();
 
-    static boolean isRunning = true;
 
-    public static void main(String[] args) {
 
+    public Calculator() {
         operatorsPrecedence.put("+", new Operator(1, OperatorName.ADD));
         operatorsPrecedence.put("-", new Operator(1, OperatorName.SUBTRACT));
-        operatorsPrecedence.put("*", new Operator(2, OperatorName.Multiply));
-        operatorsPrecedence.put("/", new Operator(2, OperatorName.Divide));
-        operatorsPrecedence.put("^", new Operator(3, OperatorName.Power));
-
-
-
-        Scanner scanner = new Scanner(System.in);
-        String line;
-
-        do {
-            line = scanner.nextLine();
-
-            if (line.startsWith("/")) {
-                parseCommand(line);
-            } else if (SINGLE_NUMBER.matcher(line).matches()) {
-                System.out.println(line);
-            } else if(!line.isEmpty()) { //TODO
-                parseExpression(line);
-            }
-        } while (isRunning);
-        System.out.println("Bye!");
-        scanner.close();
+        operatorsPrecedence.put("x", new Operator(2, OperatorName.Multiply));
+        operatorsPrecedence.put("÷", new Operator(2, OperatorName.Divide));
+        operatorsPrecedence.put("%", new Operator(3, OperatorName.PERCENTAGE));
     }
 
-    static void parseCommand(String line) {
+    public String calculate(String line) {
 
-        if (line.equals(HELP)) {
-            System.out.println("The program performs calculation operations.");
-        } else if (line.equals(EXIT)) {
-            isRunning = false;
-        }
-
-    }
-
-
-    static void parseExpression(String line) {
         Deque<String> postfix = getPostfix(line);
 
-
         if (postfix.isEmpty()) {
-            System.out.println("Invalid expression");
+            return "Invalid expression";
         } else {
             Deque<String> result = new ArrayDeque<>();
 
@@ -73,98 +43,89 @@ public class Calculator {
 
                 if (SINGLE_NUMBER.matcher(value).matches()) {
                     int negativeSignsCount = value.length() - value.replaceAll("-", "").length();
-                    result.push(negativeSignsCount %2 == 0 ? value : "-".concat(value.replaceAll("[+-]+", "")));
+                    result.push(negativeSignsCount % 2 == 0 ? value : "-".concat(value.replaceAll("[+-]+", "")));
                 } else if (OPERATOR.matcher(value).matches()) {
-                    BigInteger b = new Scanner(result.pop()).nextBigInteger();
-                    BigInteger a = new Scanner(result.pop()).nextBigInteger();
+                    Double b = Double.valueOf(result.pop());
+                    Double a = Double.valueOf(result.pop());
 
                     OperatorName operator = operatorsPrecedence.get(value).operatorName;
 
                     switch (operator) {
                         case ADD:
-                            result.push(String.valueOf(a.add(b)));
+                            result.push(String.valueOf(a + b));
                             break;
 
                         case SUBTRACT:
-                            result.push(String.valueOf(a.subtract(b)));
+                            result.push(String.valueOf(a - b));
                             break;
 
                         case Multiply:
-                            result.push(String.valueOf(a.multiply(b)));
+                            result.push(String.valueOf(a * b));
                             break;
 
                         case Divide:
-                            result.push(String.valueOf(a.divide(b)));
+                            result.push(String.valueOf(a / b));
                             break;
+                        case PERCENTAGE:
+                            result.push(String.valueOf((a * b) / 100));
 
                     }
                 }
             }
-            System.out.println(result.pop());
+            return result.pop();
         }
-
     }
 
-    static Deque<String> getPostfix(String infix) {
+    Deque<String> getPostfix(String infix) {
 
         Deque<String> result = new ArrayDeque<>();
         Deque<String> stack = new ArrayDeque<>(); //rename to operatorsStack
 
-        int leftParenthesisCount = infix.length() - infix.replaceAll(LEFT_PARENTHESIS.toString(), "").length();
-        int rightParenthesisCount = infix.length() - infix.replaceAll(RIGHT_PARENTHESIS.toString(), "").length();
 
-        if (leftParenthesisCount != rightParenthesisCount || infix.matches(".*\\*{2,}.*|.*/{2,}.*")) {
-            return new ArrayDeque<>();
-        }
 
-        infix = infix.replaceAll(String.valueOf(LEFT_PARENTHESIS), " ( ");
-        infix = infix.replaceAll(String.valueOf(RIGHT_PARENTHESIS), " ) ");
-        infix = infix.replaceAll("\\++", " + ");
-        infix = infix.replaceAll("\\*+", " * ");
-        infix = infix.replaceAll("\\^", " ^ ");
-
-        infix = infix.replaceAll(String.valueOf(RIGHT_PARENTHESIS), " ) ");
-        infix = infix.replaceAll(String.valueOf(RIGHT_PARENTHESIS), " ) ");
         Scanner sc = new Scanner(infix);
 
         while (sc.hasNext()) {
             String value = sc.next();
 
             if (SINGLE_NUMBER.matcher(value).matches()) {
-                result.offer(value);
+                result.addLast(value);
             } else if (LEFT_PARENTHESIS.matcher(value).matches()) {
                 stack.push(value);
             } else if (OPERATOR.matcher(value).matches()) {
-                if (value.matches("[+-]{2,}")) {
-                    int negativesCount = value.length() - value.replaceAll("-", "").length();
-                    value = negativesCount % 2 == 0 ? "+" : "-";
-                }
 
                 if (stack.isEmpty() || stack.peek().equals("(")) {
                     stack.push(value);
                 } else {
                     String stackTop = stack.peek();
+
                     int topItemPrecedence = operatorsPrecedence.get(stackTop).precedence;
                     int incomingItemPrecedence = operatorsPrecedence.get(value).precedence;
 
                     if (incomingItemPrecedence > topItemPrecedence) {
                         stack.push(value);
                     } else {
-                        while (stackTop != null && !stackTop.equals("(")) {
+                        while (stackTop != null && (!stackTop.equals("(") && incomingItemPrecedence <= topItemPrecedence) ) {
                             String item = stack.pop();
                             result.offer(item);
+
                             stackTop = stack.peek();
+                            System.out.println("Stack top:" + stackTop);
+                            topItemPrecedence = stackTop != null ?operatorsPrecedence.get(stackTop).precedence : -1;
                         }
                         stack.push(value);
                     }
                 }
-            } else if (RIGHT_PARENTHESIS.matcher(value).matches()) {
+            }  else if(RIGHT_PARENTHESIS.matcher(value).matches()) {
 
-                String stackTop = stack.pop();
+                String stackTop = "";
 
                 do {
-                    result.offer(stackTop);
                     stackTop = stack.pop();
+                    if (!LEFT_PARENTHESIS.matcher(stackTop).matches() && !RIGHT_PARENTHESIS.matcher(stackTop).matches()) {
+                        result.offer(stackTop);
+
+                    }
                 } while (!stackTop.equals("("));
             }
         }
@@ -189,5 +150,5 @@ class Operator {
 }
 
 enum OperatorName {
-    ADD, SUBTRACT, Multiply, Divide, Power
+    ADD, SUBTRACT, Multiply, Divide, PERCENTAGE
 }
